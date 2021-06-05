bf09a7bb0eedab282cdbec5cec30372a21112698
GuiCelebrini <celebrinig@gmail.com>
2021-06-04 23:11:10 -0300

    Todos os tutoriais finalizados

--- APS2 e MA2 - Web III/mysite/polls/models.py
+++ APS2 e MA2 - Web III/mysite/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.utils import timezone
 from django.db import models
 
@@ -7,6 +8,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
 
     def was_published_recently(self):
         now = timezone.now()
